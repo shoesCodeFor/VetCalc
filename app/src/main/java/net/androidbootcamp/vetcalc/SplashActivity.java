8bd055842db43ae1a6ca66beb8d3501609696d1f
Schuyler Ankele <Shoe@Schuylers-Mac-Pro.local>
2017-03-14 14:15:38 -0600

    Initial commit

---
+++ app/src/main/java/net/androidbootcamp/vetcalc/SplashActivity.java
@@ -0,0 +1,28 @@
+package net.androidbootcamp.vetcalc;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TimerTask task = new TimerTask(){
+
+
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        };
+        Timer openeing = new Timer();
+        openeing.schedule(task, 5000);
+    }
+}
